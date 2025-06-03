28adf28c7c7af6e6bc9cc7b52ac05ba7f3f25390
LeoYang2001 <jya261@uky.edu>
2025-06-03 16:39:28 -0400

    patch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-    theme: {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
     extend: {
       keyframes: {
         shake: {
@@ -11,12 +8,19 @@ module.exports = {
           '25%': { transform: 'translateY(-4px)' },
           '75%': { transform: 'translateY(4px)' },
         },
+        shakePause: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '5%': { transform: 'translateX(-4px)' },
+          '10%': { transform: 'translateX(4px)' },
+          '15%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(0)' },
+        }
       },
       animation: {
         shake: 'shake 1.5s ease-in-out infinite',
+        shakePause: 'shakePause 4s ease-in-out infinite', // shake quickly, rest longer
       },
     },
   },
   plugins: [],
 }
-
